95cfff062c5554f5d62a5abac7fbc99baa65f0c9
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-12-22 00:35:55 +0530

    change body backgroud

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
-      return <main>Welcome</main>;
+      return (
+            <>
+                  <main>Welcome</main>
+                  <Button>Button</Button>
+            </>
+      );
 }
