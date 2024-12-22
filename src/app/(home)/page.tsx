3c8a1d326d24813d91850942b62697ff33712b12
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-12-22 11:41:13 +0530

    add hero section

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,26 @@
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+
+export default function Home() {
+      return (
+            <>
+                  <section className="bg-white">
+                        <div className="contianer max-w-screen-xl mx-auto flex items-center justify-between py-24">
+                              <div className="text-7xl font-black font-sans leading-2">
+                                    <h1>
+                                          Saver Delicious Pizza in <br />
+                                          <span className="text-primary">Only 45 Mintures!</span>
+                                    </h1>
+                                    <p className="text-2xl mt-8 max-w-lg leading-sung">
+                                          Enjoy a Free Meal if Your Order Takes More Than 45 Mintures!
+                                    </p>
+                                    <Button className="mt-8 text-lg rounded-full  py-7 px-6 font-bold">Get you pizza now</Button>
+                              </div>
+                              <div>
+                                    <Image alt="pizza" src={'/pizza.png'} width={400} height={400} />
+                              </div>
+                        </div>
+                  </section>
+            </>
+      );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import {Manrope} from 'next/font/google';
 import { cn } from "@/lib/utils";
+import Header from "@/components/custom/header";
 
 // const geistSans = localFont({
 //   src: "./fonts/GeistVF.woff",
@@ -35,6 +36,7 @@ export default function RootLayout({
       <body
         className={cn('min-h-screen bg-backgroud font-manrope antialiased', manrope.variable)}
       >
+        <Header />
         {children}
       </body>
     </html>
