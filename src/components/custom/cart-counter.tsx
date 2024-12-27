0581fbd4c10b55673eabf082957bb303e0b3af1b
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-12-28 01:27:20 +0530

    update cart counter to display the number of items in the cart

--- src/components/custom/cart-counter.tsx
+++ src/components/custom/cart-counter.tsx
@@ -1,17 +1,18 @@
 'use client';
-import { useAppDispatch, useAppSelector } from '@/lib/store/hooks';
+import { useAppSelector } from '@/lib/store/hooks';
 import { ShoppingBasket } from 'lucide-react';
 import Link from 'next/link';
 import React from 'react';
 
 const CartCounter = () => {
+      const cartItem = useAppSelector((state) => state.cart.cartItem);
       return (
             <div className="relative">
                   <Link href={'/cart'}>
                         <ShoppingBasket className="hover:text-primary" />
                   </Link>
                   <span className="absolute -top-4 -right-5 h-6 w-6 flex items-center justify-center rounded-full bg-primary font-bold text-white">
-                        0
+                        {cartItem.length}
                   </span>
             </div>
       );
