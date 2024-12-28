45ccd94b3343dfddf5b3506cb97342b3bbd1f4f4
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-12-29 00:42:21 +0530

    create comman getTotalPrice function

--- src/lib/custom-hooks/useTotalPrice.tsx
+++ src/lib/custom-hooks/useTotalPrice.tsx
@@ -1,21 +1,10 @@
 import { useMemo } from 'react';
 import { CartItem } from '../store/features/cart/cartSlice';
+import { getTotalPrice } from '../utils';
 
 export function useTotalPrice(product: CartItem) {
     const totalPrice = useMemo(() => {
-        const toppingTotal =
-            product.chosenConfiguration.selectedToppings.reduce(
-                (acc, curr) => acc + Number(curr.price),
-                0
-            );
-        const configPricing = Object.entries(
-            product.chosenConfiguration.priceConfiguration
-        ).reduce((acc, [key, value]) => {
-            const price =
-                product.priceConfiguration[key].availableOptions[value];
-            return acc + Number(price);
-        }, 0);
-        return toppingTotal + configPricing;
+        return getTotalPrice(product);
     }, [product]);
     return totalPrice;
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,36 +1,58 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-import { CartItem } from "./store/features/cart/cartSlice"
-import CryptoJS from "crypto-js"
-import { Product } from "./types"
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { CartItem } from './store/features/cart/cartSlice';
+import CryptoJS from 'crypto-js';
+import { Product } from './types';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs));
 }
 
-export function hashTheItem(payload:CartItem): string{
-  //hashing
-  const jsonString = JSON.stringify({...payload, qty: undefined})
-  const hash = CryptoJS.SHA256(jsonString).toString();
-  return hash;
+export function hashTheItem(payload: CartItem): string {
+    //hashing
+    const jsonString = JSON.stringify({ ...payload, qty: undefined });
+    const hash = CryptoJS.SHA256(jsonString).toString();
+    return hash;
 }
 
-export function getFromPrice(product:Product){
-  const basePrice = Object.entries(product.priceConfiguration).filter(([key,value])=>{
-    return value.priceType === 'base'
-  }).reduce((acc, [key,value])=>{
-    const smallestPrice = Math.min(...Object.values(value.availableOptions))
-    return acc + smallestPrice
-  },0)
-  return basePrice;
+export function getFromPrice(product: Product) {
+    const basePrice = Object.entries(product.priceConfiguration)
+        .filter(([key, value]) => {
+            return value.priceType === 'base';
+        })
+        .reduce((acc, [key, value]) => {
+            const smallestPrice = Math.min(
+                ...Object.values(value.availableOptions)
+            );
+            return acc + smallestPrice;
+        }, 0);
+    return basePrice;
 }
 
 export function cartConfig(cartItem: CartItem) {
-  const config = Object.values(cartItem.chosenConfiguration.priceConfiguration).join(', ')
-  return config
+    const config = Object.values(
+        cartItem.chosenConfiguration.priceConfiguration
+    ).join(', ');
+    return config;
 }
 
-export function cartSelectedToppings(cartItem: CartItem){
-  const config = cartItem.chosenConfiguration.selectedToppings.map((item)=>item.name).join(', ')
-  return config
+export function cartSelectedToppings(cartItem: CartItem) {
+    const config = cartItem.chosenConfiguration.selectedToppings
+        .map((item) => item.name)
+        .join(', ');
+    return config;
+}
+
+export function getTotalPrice(product: CartItem) {
+    const toppingTotal = product.chosenConfiguration.selectedToppings.reduce(
+        (acc, curr) => acc + Number(curr.price),
+        0
+    );
+    const configPricing = Object.entries(
+        product.chosenConfiguration.priceConfiguration
+    ).reduce((acc, [key, value]) => {
+        const price = product.priceConfiguration[key].availableOptions[value];
+        return acc + Number(price);
+    }, 0);
+    return toppingTotal + configPricing;
 }
