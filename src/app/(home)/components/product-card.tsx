9c68914be221e18426ea0a4e7f82705b7dc156d2
Goswami Deepak Kumar <43085937+GoswamiDeepak@users.noreply.github.com>
2024-12-28 14:05:06 +0530

    get minimum price for product cart

--- src/app/(home)/components/product-card.tsx
+++ src/app/(home)/components/product-card.tsx
@@ -1,36 +1,46 @@
-import { Card, CardContent, CardFooter, CardHeader } from '@/components/ui/card';
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+} from '@/components/ui/card';
 import { Product } from '@/lib/types';
 import ProductModel from './Product-model';
 import Photos from '@/components/custom/photos';
+import { getFromPrice } from '@/lib/utils';
 
 type PropTypes = {
-      product: Product;
+    product: Product;
 };
 const ProductCard = ({ product }: PropTypes) => {
-      return (
-            <div>
-                  <Card className="border-none rounded-xl">
-                        <CardHeader className="flex items-center justify-center">
-                              <Photos alt="pizza-img" src={product.image} width={150} height={150} />
-                        </CardHeader>
-                        <CardContent>
-                              <h2 className="text-xl font-bold">{product.name}</h2>
-                              <p className="mt-2">{product.description}</p>
-                        </CardContent>
-                        <CardFooter className="flex items-center justify-between mt-4">
-                              <p>
-                                    <span>From </span>
-                                    <span className="font-bold">
-                                          &#8377;
-                                          {/* {product.price} */}
-                                          100
-                                    </span>
-                              </p>
-                              <ProductModel product={product} />
-                        </CardFooter>
-                  </Card>
-            </div>
-      );
+    return (
+        <div>
+            <Card className="border-none rounded-xl">
+                <CardHeader className="flex items-center justify-center">
+                    <Photos
+                        alt="pizza-img"
+                        src={product.image}
+                        width={150}
+                        height={150}
+                    />
+                </CardHeader>
+                <CardContent>
+                    <h2 className="text-xl font-bold">{product.name}</h2>
+                    <p className="mt-2">{product.description}</p>
+                </CardContent>
+                <CardFooter className="flex items-center justify-between mt-4">
+                    <p>
+                        <span>From </span>
+                        <span className="font-bold">
+                            &#8377;
+                            {getFromPrice(product)}
+                        </span>
+                    </p>
+                    <ProductModel product={product} />
+                </CardFooter>
+            </Card>
+        </div>
+    );
 };
 
 export default ProductCard;
